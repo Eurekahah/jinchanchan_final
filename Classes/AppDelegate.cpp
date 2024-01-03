cf0af1ac8df1259146711ab551f9ca7ad5c1d676
LucasFrancis23213 <143398884+LucasFrancis23213@users.noreply.github.com>
2024-01-03 09:42:17 +0800

    Add files via upload
    Final Version

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,29 +1,17 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
+//#define hanjingxiao
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+//#include "HelloWorldScene.h"
+#include"Player/PlayerManager.h"
+#include"Scene/Loading_page.h"
+#include"Scene/FashionShow.h"
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#ifdef hanjingxiao
+//#include"GameController.h"
+#endif // hanjingxiao
+
 
 // #define USE_AUDIO_ENGINE 1
 
@@ -38,6 +26,7 @@ static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+
 
 AppDelegate::AppDelegate()
 {
@@ -73,9 +62,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("project1", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("HelloWorld", cocos2d::Rect(0, 0, mediumResolutionSize.width, mediumResolutionSize.height));
 #else
-        glview = GLViewImpl::create("project1");
+        glview = GLViewImpl::create("HelloWorld");
 #endif
         director->setOpenGLView(glview);
     }
@@ -87,7 +76,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0f / 60);
 
     // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    glview->setDesignResolutionSize(mediumResolutionSize.width, mediumResolutionSize.height, ResolutionPolicy::NO_BORDER);
     auto frameSize = glview->getFrameSize();
     // if the frame's height is larger than the height of medium size.
     if (frameSize.height > mediumResolutionSize.height)
@@ -107,11 +96,25 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     register_all_packages();
 
-    // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    //plist文件缓存加载
+    SpriteFrameCache::getInstance()->addSpriteFramesWithFile("Player/PlayerWalk.plist");
+    SpriteFrameCache::getInstance()->addSpriteFramesWithFile("HPMP/HPMP.plist");
+    SpriteFrameCache::getInstance()->addSpriteFramesWithFile("armors.plist");//在init方法中写
+    SpriteFrameCache::getInstance()->addSpriteFramesWithFile("runes.plist");
+    
+    
+        //Player玩家列表加载
+        auto playermanager = PlayerManager::getInstance();
 
-    // run
-    director->runWithScene(scene);
+        // 开始界面初始化
+        auto scene = Loading_page::createScene();
+
+        //auto scene = FashionShow::createScene();
+
+        // 运行开始界面
+        director->runWithScene(scene);
+    
+    
 
     return true;
 }
